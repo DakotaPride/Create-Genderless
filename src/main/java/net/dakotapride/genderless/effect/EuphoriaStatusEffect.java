7e640fd4e62ff356aa7d73f98c57a2ea967ba318
Dakota <rjtube13@gmail.com>
2025-05-19 00:02:35 -0500

    Adjust Euphoria status effect

--- src/main/java/net/dakotapride/genderless/effect/EuphoriaStatusEffect.java
+++ src/main/java/net/dakotapride/genderless/effect/EuphoriaStatusEffect.java
@@ -22,7 +22,7 @@ public class EuphoriaStatusEffect extends MobEffect {
         }
 
         final List<Monster> list = livingEntity.level().getEntitiesOfClass(Monster.class,
-                livingEntity.getBoundingBox().inflate(4F * amplifier), Objects::nonNull);
+                livingEntity.getBoundingBox().inflate(4F * (amplifier + 1)), Objects::nonNull);
         list.forEach(monster -> {
             if (livingEntity instanceof Player player) {
                 if (monster.getTarget() == player) {
